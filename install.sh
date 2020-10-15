3f401769846b9b1aa0f326959c24a4c4fa79abec
Antoine Hébert <hantoine02@gmail.com>
2020-10-14 22:46:17 -0400

    Now automatically use all subnets in current region

--- install.sh
+++ install.sh
@@ -1,4 +1,5 @@
 cp -v aws-* /usr/local/bin/
 mkdir -p /etc/aws-cli-utils/
 cp -iv default-aws-request.json.ejs /etc/aws-cli-utils/
+cp -iv gpu-small-aws-request.json.ejs /etc/aws-cli-utils/
 cp -iv ami-users /etc/aws-cli-utils/
