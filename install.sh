25a55960bf5f819d8dfa702a5a6acf0133cdfe5f
Antoine Hebert <antoine.hebert@glovoapp.com>
2021-12-30 21:51:49 +0100

    Create v2 of aws-request tool
    New versio no longer use Spot Fleet request because they don't support stopping an instance, which can be practical to stop/start working on a project.
    New version implemented in Python for better readability and maintainability.
    Ability to pick cheapest AZ/instance type will be implemented later

--- install.sh
+++ install.sh
@@ -3,3 +3,4 @@ mkdir -p /etc/aws-cli-utils/
 cp -iv default-aws-request.json.ejs /etc/aws-cli-utils/
 cp -iv gpu-small-aws-request.json.ejs /etc/aws-cli-utils/
 cp -iv ami-users /etc/aws-cli-utils/
+ln -s /usr/local/bin/aws-request-v1 /usr/local/bin/aws-request
